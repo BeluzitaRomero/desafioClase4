4613e83f3cd45adff440c8be60bf95131ad35e68
Belen Romero <romero.abelen@gmail.com>
2022-03-14 01:35:50 -0300

    .

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 
 const productsRouter = require("./src/routes/productRouter");
 
-//para usar post hay que usar un middleware
-//esto formatea la informacion que me envien desde un cliente
-app.use(express.json());
-
-//middlewares es una funcion que se ejecutan primero que todo
 //Routes
 app.use("/api/products", productsRouter);
 
